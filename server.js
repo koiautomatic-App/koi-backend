c77f55b06d9ce8c8b5aab7e951916f47f9de9675
koiautomatic-App <koi.automatic@gmail.com>
2026-04-16 20:40:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -951,7 +951,7 @@ const requireAuthAPI = (req, res, next) => {
 passport.use(new GoogleStrategy({
   clientID:     process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL:  `${BASE}/auth/google/callback`,
+  callbackURL:  `${process.env.BASE_URL}/auth/google/callback`,
 }, async (_, __, profile, done) => {
   try {
     const email = profile.emails?.[0]?.value?.toLowerCase();
